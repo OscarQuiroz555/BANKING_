8ecef0e67e7f88fcb186240835eb832148318d66
Oscar Quiroz <oscarquiroz118@gmail.com>
2025-10-03 23:04:40 -0500

    Country templates for UI

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
-# urls.py
 from django.urls import path
 from . import views
 
+app_name = 'banking_app'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('countries/', views.country_list, name='country'),
-    path('departments/', views.department_list, name='department'),
-    path('cities/', views.city_list, name='city'),
+    path('countries/', views.country_list, name='country_list'),
+    path('countries/create/', views.country_create, name='country_create'),
+    path('countries/<int:pk>/edit/', views.country_edit, name='country_edit'),
+    path('countries/<int:pk>/delete/', views.country_delete, name='country_delete'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,22 +1,46 @@
-# views.py
-from django.shortcuts import render
-from .models import Country, Department, City
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.core.paginator import Paginator
+from .models import Country
+from .forms import CountryForm
 
-# Página de inicio
-def index(request):
-    return render(request, 'index.html')  # Aquí puedes crear un archivo index.html en templates
-
-# Listado de países
 def country_list(request):
-    countries = Country.objects.all()
-    return render(request, 'countries.html', {'countries': countries})
+    search = request.GET.get('search', '')
+    countries = Country.objects.filter(name__icontains=search).order_by('name')
+    
+    paginator = Paginator(countries, 10)  # 10 por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'banking_app/country_list.html', {'countries': page_obj, 'search': search})
 
-# Listado de departamentos
-def department_list(request):
-    departments = Department.objects.all()
-    return render(request, 'departments.html', {'departments': departments})
+def country_create(request):
+    if request.method == 'POST':
+        form = CountryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'País creado correctamente.')
+            return redirect('banking_app:country_list')
+    else:
+        form = CountryForm()
+    return render(request, 'banking_app/country_form.html', {'form': form, 'country': None})
 
-# Listado de ciudades
-def city_list(request):
-    cities = City.objects.all()
-    return render(request, 'cities.html', {'cities': cities})
+def country_edit(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    if request.method == 'POST':
+        form = CountryForm(request.POST, instance=country)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'País actualizado correctamente.')
+            return redirect('banking_app:country_list')
+    else:
+        form = CountryForm(instance=country)
+    return render(request, 'banking_app/country_form.html', {'form': form, 'country': country})
+
+def country_delete(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    if request.method == 'POST':
+        country.delete()
+        messages.success(request, 'País eliminado correctamente.')
+        return redirect('banking_app:country_list')
+    return render(request, 'banking_app/country_delete.html', {'country': country})
